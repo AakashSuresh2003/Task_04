df57ec5ae46f3410ffe220f2a2988e2b608035c0
Aakash <aakashsuresh455@gmail.com>
2024-09-29 23:24:22 +0530

    Deleted

--- terraform/modules/ansible/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  type        = string
-}
-
-variable "key_name" {
-  description = "Name of the SSH key pair"
-  type        = string
-}
